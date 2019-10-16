830d58dc5013477459db35b3a988eeb706760507
Leonardo Vargas <leu1607@hotmail.com>
2019-10-16 10:21:09 -0300

    Fazendo ajustes no desafio 1, otimizando a busca do project

--- ATIVIDADES/desafio-01/index.js
+++ ATIVIDADES/desafio-01/index.js
@@ -3,35 +3,34 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
-
 const projects = []; // tupla = { id: "1", title: 'Novo projeto', tasks: [] };
 let resquestsAPP = 0;
 
+// Contador de nº de requisições
 server.use((req, res, next) => {
   resquestsAPP++;
   console.log(`Requisição nº: ${resquestsAPP}, Método: ${req.method}`);
   return next();
 });
 
+// Busca o um projeto apartir do id, caso não encontrar retorna o erro 400 e interrompe a aplicação
 function checkProject(req, res, next){
   const { id } = req.params;
-  let project = "";
   
-  for(p in projects){
-    if(projects[p].id === Number(id)) project = p;
-  }
+  const project = projects.findIndex(p => p.id == id);
 
-  if(!projects[project]){
+  if(project === -1){ // Se o projeto não for encontrado o retorno do findIndex é -1
     return res.status(400).json({error: "Projeto não encontrado"});
   }
+  
   req.params.indexProject = project;
   next();
 };
 
 // Novo Projeto
 server.post('/projects', (req, res) => {
-  const { id, title, tasks } = req.body;
-  projects.push({ id, title, tasks });
+  const { id, title } = req.body;
+  projects.push({ id, title, tasks: [] });
   return res.json(projects);
 });
 
